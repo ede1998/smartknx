4dea95272d91be88fe7f30aed42819f6233dd19f
ede1998 <online@erik-hennig.me>
2019-02-13 01:24:04 +0100

    devices are now constructed as objects

--- knxmessages/container.py
+++ knxmessages/container.py
@@ -1,9 +1,12 @@
+import oyaml as yaml
 
-class Container:
-    def __init__(self, name):
+class Container(yaml.YAMLObject):
+    yaml_tag = u'!Container'
+
+    def __init__(self, name, children):
         super().__init__()
         self.name = name
-        self.children = []
+        self.children = children
     
     def __repr__(self):
         return '{ name: ' + self.name + ', children: ' + repr(self.children) + '}'
--- knxmessages/objects.py
+++ knxmessages/objects.py
@@ -9,7 +9,6 @@ class Object(yaml.YAMLObject):
     def __repr__(self):
         return "%s(name=%r)" % (self.__class__.__name__, self.name)
 
-
 class Light(Object):
     yaml_tag = u'!Light'
 
@@ -18,26 +17,32 @@ class Light(Object):
         self.read = read
         self.write = write
 
+    @classmethod
+    def from_yaml(cls, loader, node):
+        return cls(**loader.construct_mapping(node))
+
     def __repr__(self):
         return "%s(name=%r, read=%r, write=%r)" % (
             self.__class__.__name__, self.name, self.read, self.write)
 
-
 class Outlet(Object):
     yaml_tag = u'!Outlet'
-
+    
     def __init__(self, read, write, name='Outlet'):
         super().__init__(name)
         self.read = read
         self.write = write
 
+    @classmethod
+    def from_yaml(cls, loader, node):
+        return cls(**loader.construct_mapping(node))
+
     def __repr__(self):
         return "%s(name=%r, read=%r, write=%r)" % (
             self.__class__.__name__, self.name, self.read, self.write)
 
-
 class BlindOld(Object):
-    yaml_tag = u'BlindOld'
+    yaml_tag = u'!BlindOld'
 
     def __init__(self, read_top, read_bottom, write_direction, write_stop, name='Blinds'):
         super().__init__(name)
@@ -46,15 +51,26 @@ class BlindOld(Object):
         self.write_direction = write_direction
         self.write_stop = write_stop
 
+    @classmethod
+    def from_yaml(cls, loader, node):
+        return cls(**loader.construct_mapping(node))
 
 #class GarageDoor(BlindOld):
     #def __init__(self, name='Garage'):
         #super().__init__(name)
 
 class Blind(Object):
+    yaml_tag = u'!Blind'
     def __init__(self, read_position, write_position, write_direction, write_stop, name='Blinds'):
         super().__init__(name)
         self.read_position = read_position
         self.write_position = write_position
         self.write_direction = write_direction
         self.write_stop = write_stop
+
+    @classmethod
+    def from_yaml(cls, loader, node):
+        return cls(**loader.construct_mapping(node))
+
+for subclass in Object.__subclasses__():
+    yaml.add_path_resolver(subclass.__dict__['yaml_tag'], ['buildings', None, 'floors', None, 'rooms', None, 'devices', None, subclass.__name__])
