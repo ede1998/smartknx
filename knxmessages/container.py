53f935b5303e38ec7684976909d283cfd9f133ef
ede1998 <online@erik-hennig.me>
2019-02-14 23:14:19 +0100

    Objects are now created from yaml config, works finally

--- knxmessages/container.py
+++ knxmessages/container.py
@@ -1,15 +1,60 @@
 import oyaml as yaml
 
+
 class Container(yaml.YAMLObject):
     yaml_tag = u'!Container'
 
-    def __init__(self, name, children):
+    def __init__(self, url, children, name='Container'):
+        super().__init__()
+        self.name = name
+        self.url = url
+        self.children = children
+
+    @classmethod
+    def from_yaml(cls, loader, node):
+        m = loader.construct_mapping(node)
+
+        # rename some keys, so it fits into container
+        def _get_list_key():
+            for poss_key in ['buildings', 'floors', 'rooms']:
+                if poss_key in m:
+                    return poss_key
+
+        m['children'] = m.pop(_get_list_key())
+        if 'project' in m:
+            m['name'] = m['url'] = m.pop('project')
+
+        return cls(**m)
+
+    def __repr__(self):
+        return '{ name: %s, url: %s, children: %s}' % (self.name, self.url, repr(self.children))
+
+
+class Box(yaml.YAMLObject):
+    yaml_tag = u'!Box'
+
+    def __init__(self, children, name='Room'):
         super().__init__()
         self.name = name
         self.children = children
-    
+
+    @classmethod
+    def from_yaml(cls, loader, node):
+        m = loader.construct_mapping(node)
+        print(m)
+        m['children'] = m.pop('devices')
+        return cls(**m)
+
     def __repr__(self):
-        return '{ name: ' + self.name + ', children: ' + repr(self.children) + '}'
-    
-    def __str__(self):
-        return 'Containername: ' + self.name + '\n' + str(self.children)
+        return '{ name: %s, children: %s}' % (self.name, repr(self.children))
+
+
+_path_containers = ['buildings', None,
+                    'floors', None]
+
+_path_objects = _path_containers + ['rooms', None]
+
+yaml.add_path_resolver(Box.yaml_tag, _path_objects)
+
+for i in range(0, len(_path_containers) + 1, 2):
+    yaml.add_path_resolver(Container.yaml_tag, _path_containers[:i])
--- knxmessages/objects.py
+++ knxmessages/objects.py
@@ -1,3 +1,4 @@
+import itertools
 import oyaml as yaml
 
 
@@ -8,6 +9,7 @@ class Object(yaml.YAMLObject):
 
     def __repr__(self):
         return "%s(name=%r)" % (self.__class__.__name__, self.name)
+
 
 class Light(Object):
     yaml_tag = u'!Light'
@@ -25,9 +27,10 @@ class Light(Object):
         return "%s(name=%r, read=%r, write=%r)" % (
             self.__class__.__name__, self.name, self.read, self.write)
 
+
 class Outlet(Object):
     yaml_tag = u'!Outlet'
-    
+
     def __init__(self, read, write, name='Outlet'):
         super().__init__(name)
         self.read = read
@@ -40,6 +43,7 @@ class Outlet(Object):
     def __repr__(self):
         return "%s(name=%r, read=%r, write=%r)" % (
             self.__class__.__name__, self.name, self.read, self.write)
+
 
 class BlindOld(Object):
     yaml_tag = u'!BlindOld'
@@ -59,8 +63,10 @@ class BlindOld(Object):
     #def __init__(self, name='Garage'):
         #super().__init__(name)
 
+
 class Blind(Object):
     yaml_tag = u'!Blind'
+
     def __init__(self, read_position, write_position, write_direction, write_stop, name='Blinds'):
         super().__init__(name)
         self.read_position = read_position
@@ -72,5 +78,17 @@ class Blind(Object):
     def from_yaml(cls, loader, node):
         return cls(**loader.construct_mapping(node))
 
+
+_path_objects = ['buildings', None,
+                 'floors', None,
+                 'rooms', None,
+                 'devices', None]
+
+# remove types: Light, Blind, ...
+yaml.add_constructor('!device', lambda loader, node:
+                     list(loader.construct_mapping(node).values())[0])
+yaml.add_path_resolver('!device', _path_objects)
+
 for subclass in Object.__subclasses__():
-    yaml.add_path_resolver(subclass.__dict__['yaml_tag'], ['buildings', None, 'floors', None, 'rooms', None, 'devices', None, subclass.__name__])
+    yaml.add_path_resolver(subclass.__dict__['yaml_tag'],
+                           itertools.chain(_path_objects, [subclass.__name__]))
