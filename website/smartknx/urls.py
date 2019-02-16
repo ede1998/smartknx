e926c6123f4e0821136ef1dd7f3944b069c0e8d3
ede1998 <online@erik-hennig.me>
2019-02-16 15:42:42 +0100

    removed dependency to functools.partial: django supports needed functionality out of the box

--- website/smartknx/urls.py
+++ website/smartknx/urls.py
@@ -16,14 +16,13 @@ Including another URLconf
 from django.urls import path
 from . import views
 from . import configuration
-from functools import partial
 
 urlpatterns = [
     path('', views.navigation, name='root'),
 ]
 
 for area in configuration.children:
-    urlpatterns.append(path(area.url, partial(views.navigation, area=area), name=area.id))
+    urlpatterns.append(path(area.url,views.navigation, { 'area': area }, name=area.id))
     for subarea in area.children:
         url = area.url + '/' + subarea.url
-        urlpatterns.append(path(url, partial(views.details, area=subarea), name=subarea.id))
+        urlpatterns.append(path(url, views.details, { 'area': subarea }, name=subarea.id))
--- website/smartknx/views.py
+++ website/smartknx/views.py
@@ -4,11 +4,9 @@ from . import configuration
 # Create your views here.
 
 def details(request, **kwargs):
-    print(kwargs['area'])
     ctx = {'all': configuration.__dict__, 'current': kwargs['area']}
     return render(request, 'smartknx/details.html', ctx)
 
 def navigation(request, **kwargs):
-    print(kwargs.get('area', configuration))
     ctx = {'all': configuration.__dict__, 'current': kwargs.get('area', configuration)}
     return render(request, 'smartknx/navigation.html', ctx)
